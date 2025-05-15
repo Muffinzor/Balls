cfa970eea818ee98fac2eed8a9edfc0f9376b69f
Leonardo Uliano <uliano.leonardo@courrier.uqam.ca>
2025-05-15 14:06:32 -0400

    world init and step

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <box2d/box2d.h>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML + Box2D");
+
+    b2Vec2 gravity(0.f, -9.8f);
+    b2World world(gravity);
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.f, -10.f);
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.f, 10.f);
+    groundBody->CreateFixture(&groundBox, 0.f);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        world.Step(1.0f / 60.0f, 8, 3);
+
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
     return 0;
 }
